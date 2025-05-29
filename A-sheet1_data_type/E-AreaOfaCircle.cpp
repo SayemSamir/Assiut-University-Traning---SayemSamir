a73468a812390b242a1fbdfab287321882dfcfa0
Sayem Uddin Samir <sayemuddinsamir00@gmail.com>
2025-05-29 13:26:53 +0600

    Update E-AreaOfaCircle.cpp

---
+++ A-sheet1_data_type/E-AreaOfaCircle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<iomanip>
+ using namespace std;
+int main(){
+  const double PI = 3.141592653;
+  double R;
+  cin>>R;
+   cout << fixed << setprecision(9) << PI * R * R <<endl;
+}
