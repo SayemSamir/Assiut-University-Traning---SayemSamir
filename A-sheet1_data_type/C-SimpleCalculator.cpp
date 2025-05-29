a0f950a92fea68cedfaaba7b61ed8fca8b8d7406
Sayem Uddin Samir <sayemuddinsamir00@gmail.com>
2025-05-29 13:17:56 +0600

    Update C-SimpleCalculator.cpp

--- A-sheet1_data_type/C-SimpleCalculator.cpp
+++ A-sheet1_data_type/C-SimpleCalculator.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
   long num1,num2;
   cin>>num1>>num2;
-  cout<<num1<<" + "<<num2<<" = "<<num1+num2<<endl;
-  cout<<num1<<" * "<<num2<<" = "<<num1*num2<<endl;
-  cout<<num1<<" - "<<num2<<" = "<<num1-num2<<endl;
+  cout<< num1 <<" + "<< num2 <<" = "<< num1+num2 <<endl;
+  cout<< num1 <<" * "<< num2 <<" = "<< num1*num2 <<endl;
+  cout<< num1 <<" - "<< num2 <<" = "<< num1-num2 <<endl;
 
 }
