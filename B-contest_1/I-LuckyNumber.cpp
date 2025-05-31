5a517a925541e8c6c221f016e5ce3db541773d0a
Sayem Uddin Samir <sayemuddinsamir00@gmail.com>
2025-05-31 14:27:19 +0600

    Update I-LuckyNumber.cpp

---
+++ B-contest_1/I-LuckyNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    short num;
+    cin >> num;
+    if (num % 10 == 0)
+    {
+        cout << "YES";
+        return 0;
+    }
+    short firstDigit = num / 10;
+    short secondDigit = num % 10;
+    (firstDigit % secondDigit == 0 || secondDigit % firstDigit == 0) ? cout << "YES" : cout << "NO";
+}
